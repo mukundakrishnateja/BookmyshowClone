4fbed4497dc59475d03532653e36fa0c4efe1481
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-16 22:10:15 +0530

    some changes in backend and frontend files, including package.json, .env, and vercel.json.

---
+++ server/inngest/index.js
@@ -0,0 +1,53 @@
+import User from "../models/User.js";
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({ id: "movie-ticket-booking" });
+
+
+// Inngest function to save user data to a database
+const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk.user.created'},
+    async({event})=>{
+        const{id,first_name,last_name,email_addresses,image_url} = event.data;
+        const userData = {
+            _id: id,
+            name: `${first_name} ${last_name}`,
+            email: email_addresses[0].email_address,
+            image: image_url
+        }
+        await User.create(userData)
+        console.log(userData);
+    }
+)
+
+//inngest function to delete user from database
+
+const syncUserDeletion = inngest.createFunction(
+    {id: 'delete-user-from-clerk'},
+    {event: 'clerk.user.deleted'},
+    async({event})=>{
+        const {id} = event.data;
+        await User.findByIdAndDelete(id);
+        console.log(`User with id ${id} deleted from database`);
+    }
+)
+
+// Inngest function to update user data in the database when user updates their profile in Clerk
+const syncUserUpdate = inngest.createFunction(
+    {id: 'update-user-in-clerk'},
+    {event: 'clerk.user.updated'},
+    async({event})=>{
+        const {id,first_name,last_name,email_addresses,image_url} = event.data;
+        const userData ={
+            _id:id,
+            email: email_addresses[0].email_address,
+            name: `${first_name} ${last_name}`,
+            image: image_url
+        }
+        await User.findByIdAndUpdate(id, userData);
+        console.log(`User with id ${id} updated in database`);
+    }
+)
+
+export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdate];
--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors()); // 
-app.use(express.json());
-
 const port = 3000;
-// Load environment variables from .env file
+await connectDB();
+
+// Middleware
+app.use(cors()); 
+app.use(express.json());
+app.use(clerkMiddleware())
+
+
+// Routes
 app.get('/',(req,res)=>{
     res.send("Server is running");
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.listen(port,()=> console.log(`Server is running at http://localhost:3000/`));
