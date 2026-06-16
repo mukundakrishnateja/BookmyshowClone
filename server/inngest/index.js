2fba3e89eb6d1bad17f1b478cb5290e41fe02ac0
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-16 22:29:27 +0530

    model import fix and some syntax errors fixed

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -6,37 +6,44 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 // Inngest function to save user data to a database
 const syncUserCreation = inngest.createFunction(
-    {id: 'sync-user-from-clerk'},
-    {event: 'clerk.user.created'},
-    async({event})=>{
-        const{id,first_name,last_name,email_addresses,image_url} = event.data;
+    {
+        id: "sync-user-from-clerk",
+        triggers: [{ event: "clerk.user.created" }]
+    },
+    async ({ event }) => {
+        const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
         const userData = {
             _id: id,
             name: `${first_name} ${last_name}`,
             email: email_addresses[0].email_address,
             image: image_url
-        }
-        await User.create(userData)
+        };
+
+        await User.create(userData);
         console.log(userData);
     }
-)
+);
 
 //inngest function to delete user from database
 
 const syncUserDeletion = inngest.createFunction(
-    {id: 'delete-user-from-clerk'},
-    {event: 'clerk.user.deleted'},
-    async({event})=>{
-        const {id} = event.data;
+    {
+        id: "delete-user-from-clerk",
+        triggers: [{ event: "clerk.user.deleted" }]
+    },
+    async ({ event }) => {
+        const { id } = event.data;
         await User.findByIdAndDelete(id);
-        console.log(`User with id ${id} deleted from database`);
     }
-)
+);
 
 // Inngest function to update user data in the database when user updates their profile in Clerk
 const syncUserUpdate = inngest.createFunction(
-    {id: 'update-user-in-clerk'},
-    {event: 'clerk.user.updated'},
+    {
+        id: "update-user-in-clerk",
+        triggers: [{ event: "clerk.user.updated" }]
+    },
     async({event})=>{
         const {id,first_name,last_name,email_addresses,image_url} = event.data;
         const userData ={
