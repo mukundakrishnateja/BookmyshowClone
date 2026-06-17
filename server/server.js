6d68772380005a9bd6ee0b9e96a200341345a392
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-17 10:22:56 +0530

    made changes to server.js

--- server/server.js
+++ server/server.js
@@ -17,15 +17,9 @@ await connectDB();
 app.use(cors()); 
 app.use(express.json());    
 
-app.get("/api/inngest", (req, res) => {
-    res.json({
-        success: true,
-        message: "Reached Express!"
-    });
-});
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 
-// app.use(clerkMiddleware())
 
 
 // Routes
