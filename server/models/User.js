a10983bc3fe3202ab309d50ebb05e182d68e11fa
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-17 09:39:57 +0530

    made changes in server.js

--- server/models/User.js
+++ server/models/User.js
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema({
 })
     
 
-const User = mongoose.model('User',userSchema)
+const User =
+  mongoose.models.User ||
+  mongoose.model("User", userSchema);
 
 export default User;
