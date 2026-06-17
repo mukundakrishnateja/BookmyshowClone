24194cb024040ba326b25ae400d0483536a7de94
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-17 09:40:26 +0530

    changes some syntax errors in server.js

--- server/server.js
+++ server/server.js
@@ -29,5 +29,9 @@ app.get('/',(req,res)=>{
 })
 
 
-
-app.listen(port,()=> console.log(`Server is running at http://localhost:3000/`));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () =>
+    console.log(`Server is running at http://localhost:${port}`)
+  );
+}
+export default app;
