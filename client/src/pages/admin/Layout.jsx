6a453d38e6f2ae4e4fc49d5f4c81dee4fb29a209
mukundakrishnateja <krishnamukund2000@gmail.com>
2026-06-22 16:28:55 +0530

    Fix AdminSideBar import

--- client/src/pages/admin/Layout.jsx
+++ client/src/pages/admin/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
-import AdminSideBar from '../../components/admin/AdminSideBar'
+import AdminSideBar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 import { useAppContext } from '../../context/AppContext'
 import Loading from '../../components/Loading'
